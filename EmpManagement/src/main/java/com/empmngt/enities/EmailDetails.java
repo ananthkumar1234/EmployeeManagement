fb2146c9366aa570076b4efb3cc31620cce1be5a
ananthkumar1234 <ananthtd234@gmail.com>
2024-07-03 14:39:04 +0530

    EmailDetail Entity

---
+++ EmpManagement/src/main/java/com/empmngt/enities/EmailDetails.java
@@ -0,0 +1,28 @@
+package com.empmngt.enities;
+
+public class EmailDetails {
+	
+	private String employeeEmail;
+    private String hrEmail;
+    private String managerEmail;
+	public String getEmployeeEmail() {
+		return employeeEmail;
+	}
+	public void setEmployeeEmail(String employeeEmail) {
+		this.employeeEmail = employeeEmail;
+	}
+	public String getHrEmail() {
+		return hrEmail;
+	}
+	public void setHrEmail(String hrEmail) {
+		this.hrEmail = hrEmail;
+	}
+	public String getManagerEmail() {
+		return managerEmail;
+	}
+	public void setManagerEmail(String managerEmail) {
+		this.managerEmail = managerEmail;
+	}
+    
+
+}
